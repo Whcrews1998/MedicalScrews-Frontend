45779449dd40ad926434f55c708b030613256d27
Whcrews1998 <whcrews1998@gmail.com>
2023-11-15 16:48:05 -0500

    Created folder for our web pages.

---
+++ src/pages/SetListPage.jsx
@@ -0,0 +1,11 @@
+import "./MedicalScrew.css"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import { faTrash } from "@fortawesome/free-solid-svg-icons"
+
+
+export default function SetListPage() {
+
+    return (
+        <div><a href="./MedicalSet">List Page</a></div>
+    )
+}
