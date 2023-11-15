13773be2f8eeeb343a54b8ce983a15b5c8917a52
Whcrews1998 <whcrews1998@gmail.com>
2023-11-15 11:19:25 -0500

    Create component and basic styling for MedicalSet.

--- src/components/MedicalSet.jsx
+++ src/components/MedicalSet.jsx
@@ -1,15 +1,16 @@
-import './MedicalSet.css'
+import "./MedicalSet.css"
 import Screw from "./MedicalScrew"
 
 export default function MedicalSet({set}) {
+
+    
+
     return (
-        <div>
-            <ul>
-                Set {set.id}
-                {set.screwList.map(screw => (
-                    <Screw screw={screw}/>
-                ))}
-            </ul>
+        <div> 
+            <div className="set-name"> Set {set.id} </div>
+            {set.screwList.map(screw => (
+                <Screw screw={screw} onClick={(screw) => screw.present = screw.present? false : true}/>
+            ))}
         </div>
     )
 }
