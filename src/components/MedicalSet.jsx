863e91ed05d43fb8c9545243952c260e19fe5148
Whcrews1998 <whcrews1998@gmail.com>
2023-11-15 15:31:52 -0500

    Refactored code so MedicalSet manages list of screws and updates list accordingly.

--- src/components/MedicalSet.jsx
+++ src/components/MedicalSet.jsx
@@ -1,15 +1,35 @@
+import { useState } from "react";
+import axios from "axios";
 import "./MedicalSet.css"
 import Screw from "./MedicalScrew"
 
 export default function MedicalSet({set}) {
+
+    const [screwList, setScrewList] = useState(set.screwList);
+
+    function togglePresent(id){
+        setScrewList(screwList.map((screw) => {
+            
+            if (screw.id === id) {
+                axios.post(`http://localhost:8080/medical-set/update-present-state?id=${screw.id}&newState=${screw.present ? false:true}`).then(response => {
+                    console.log(response.data);
+                }).catch(error => {
+                    console.error(error);
+                });
+
+                return {...screw, present: !screw.present};
+            } 
+                return {...screw};
+        }));
+    }
 
     
 
     return (
         <div> 
             <div className="set-name"> Set {set.id} </div>
-            {set.screwList.map(screw => (
-                <Screw screw={screw} onClick={(screw) => screw.present = screw.present? false : true}/>
+            {screwList.map((screw, index) => (
+                <Screw screw={screw} key={index} togglePresent={togglePresent}/>
             ))}
         </div>
     )
