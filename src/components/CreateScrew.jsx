24040be926eeaf76b38ff8d6ba8d96364353060e
Whcrews1998 <whcrews1998@gmail.com>
2023-11-16 20:50:15 -0500

    Created form to gather screw data from user and pass to MedicalSet to create screw.

--- src/components/CreateScrew.jsx
+++ src/components/CreateScrew.jsx
@@ -1,19 +1,36 @@
 import "./CreateScrew.css"
+import axios from "axios";
+import { useState, useEffect } from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import { faTrash } from "@fortawesome/free-solid-svg-icons"
 
 
-export default function CreateScrew({handle}) {
+export default function CreateScrew({addScrew}) {
+
+    const [width, setWidth] = useState(0);
+    const [length, setLength] = useState(0);
 
     function handleSubmit(e) {
         e.preventDefault();
+
+        console.log("width: " + width);
+        console.log("length: " + length);
+
+        if (width == 0 || length == 0) {
+            alert("Measurments may not be zero.");
+            return false;
+        }
+
+        addScrew(width, length);
+
+        
     }
 
     return (
         <form onSubmit={handleSubmit}>
             <label>
-                <input type="text" placeholder="width"></input>
-                <input type="text" placeholder="length"></input>
+                <input type="text" placeholder="width" onChange={(e) => setWidth(e.target.value)}></input>
+                <input type="text" placeholder="length" onChange={(e) => setLength(e.target.value)}></input>
                 <button type="submit">Create</button>
             </label>
         </form>
