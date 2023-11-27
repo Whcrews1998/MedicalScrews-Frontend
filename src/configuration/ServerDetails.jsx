daa6b089f4d2a5c5a448a0c592e42b4ed4b794aa
Whcrews1998 <whcrews1998@gmail.com>
2023-11-27 17:33:51 -0500

    Fixed configuration url.

--- src/configuration/ServerDetails.jsx
+++ src/configuration/ServerDetails.jsx
@@ -1,4 +1,4 @@
 export default {
-    url: "Medical-screws-env-1.eba-pwagpa8g.us-east-2.elasticbeanstalk.com/medical-set",
+    url: "Medical-screws-env-1.eba-pwagpa8g.us-east-2.elasticbeanstalk.com",
     port: 8081
 };
