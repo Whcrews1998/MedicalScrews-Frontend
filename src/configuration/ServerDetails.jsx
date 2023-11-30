cff6f741a16952f0674a600a71778af369ca6a36
Whcrews1998 <whcrews1998@gmail.com>
2023-11-30 09:03:23 -0500

    Changed url...again.

--- src/configuration/ServerDetails.jsx
+++ src/configuration/ServerDetails.jsx
@@ -1,4 +1,4 @@
 export default {
-    url: " https://cors-everywhere.herokuapp.com/http://Medical-screws-env-1.eba-pwagpa8g.us-east-2.elasticbeanstalk.com",
+    url: " https://Medical-screws-env-1.eba-pwagpa8g.us-east-2.elasticbeanstalk.com",
     port: 8081
 };
